b403a8261dd973183d755cfc4e4b5186756ae421
Isaac <isaacraffell@hotmail.co.uk>
2025-06-02 15:21:23 +1000

    added remove/add functionality dynamically

--- script.js
+++ script.js
@@ -2,17 +2,25 @@
 const itemForm = document.querySelector('#item-form');
 const itemInput = document.querySelector('#item-input');
 const itemList = document.querySelector('#item-list');
+const clearButton = document.querySelector('#clear');
+const itemFilter = document.querySelector('#filter');
 
 function addItem(e) {
+
     e.preventDefault();
+    //Validate
     if (itemInput.value === '') {
         alert('Please add an item');
+        return;
     }
+
+    //Create list item
     const newItem = itemInput.value;
     const li = document.createElement('li');
     li.appendChild(document.createTextNode(newItem))
     li.appendChild(createButton('remove-item btn-link text-red'))
     itemList.appendChild(li);
+    checkUI();
     
 }
 
@@ -29,10 +37,40 @@ function createIcon(classes) {
     return icon;
 }
 
+//Remove item from list
+function removeItem(e) {
+    if(e.target.parentElement.classList.contains('remove-item')) {
+        if(confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+    checkUI();
+}
+//Clear button functionality
+function clearItems(){
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild);
+    }
+    checkUI();
+}
 
-
-
+function checkUI() {
+    const items = itemList.querySelectorAll('li');
+    if (items.length === 0) {
+        clearButton.style.display = 'none';
+        itemFilter.style.display = 'none';
+    } else {
+        clearButton.style.display = 'block';
+        itemFilter.style.display = 'block';
+    }
+    
+}
 
 //Event Listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearButton.addEventListener('click', clearItems);
+
+checkUI();
+
 
